e7701cc00ac61d3a2df28eabbe247b4a28277549
soncoiz02 <66171585+soncoiz02@users.noreply.github.com>
2022-03-14 16:07:21 +0700

    feat: demo room infor

--- src/components/RoomInfor/RoomInfor.jsx
+++ src/components/RoomInfor/RoomInfor.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaPen } from 'react-icons/fa'
+import './roomInfor.scss'
 
-const RoomInfor = () => {
+const listTheme = ['green', 'red', 'pink', 'purple']
+
+const RoomInfor = ({ recieveUser, roomData }) => {
+    const [nickNameVal, setNickNameVal] = useState('')
     return (
-        <div>RoomInfor</div>
+        <div className='room-infor'>
+            <div className="user">
+                <div className="avt">
+                    <img src={recieveUser.avatar} alt="" />
+                </div>
+                <div className="name">{recieveUser.nickName}</div>
+            </div>
+            <div className="setting">
+                <div className="change">
+                    <p><FaPen />Edit nicknames</p>
+                    <div className="list-user">
+                        {
+                            roomData?.users?.map((e, index) =>
+                                <div className="item" key={index}>
+                                    <div className="avt">
+                                        <img src={e.avatar} alt="" />
+                                    </div>
+                                    <input type="text" value={nickNameVal} placeholder={e.nickName} onChange={(e) => setNickNameVal(e.target.value)} />
+                                </div>
+                            )
+                        }
+                    </div>
+                    <button className="btn-apply">Apply</button>
+                </div>
+                <div className="change">
+                    <p><div className="circle"></div>Change theme</p>
+                    <div className="list-theme">
+                        {
+                            listTheme.map((e, index) =>
+                                <div className="theme" style={{ background: e }} key={index}></div>
+                            )
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
